cd07201dbc76cd2a8a1d1814d84cec3b55bdc0c6
tkawa <tkawa@4bit.net>
2012-05-22 14:35:31 +0900

    deny to edit top page

--- app/controllers/wiki_pages_controller.rb
+++ app/controllers/wiki_pages_controller.rb
@@ -2,6 +2,9 @@ class WikiPagesController < ApplicationController
   acts_as_wiki_pages_controller
 
   protected
+  def edit_allowed?
+    @page.path != '' # not allowed on top page
+  end
   def destroy_allowed?
     false
   end
